4287f6e86de61bfc25cb0cd6ef6615ccf121a759
samira <79003815+samirashad@users.noreply.github.com>
2022-01-09 18:21:19 +0330

    handle search engin

--- src/app.js
+++ src/app.js
@@ -36,14 +36,35 @@ function displayInformation(response) {
   document.querySelector("#currentTime").innerHTML = formatDate(
     response.data.dt * 1000
   );
-  console.log(
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
   document.querySelector(
     "#weatherIcon"
   ).src = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
 }
+function search(city) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayInformation);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#searchBox");
+  if (cityInput.value !== "") {
+    search(cityInput.value);
+  } else {
+    alert("enter a city");
+  }
+}
+function getCoordinates(position) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayInformation);
+}
+function handleClickCurrentBtn(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getCoordinates);
+}
 
 let apiKey = "0cade312aa440618836af6e6fd05e7ad";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=mashhad&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayInformation);
+search("mashhad");
+document.querySelector("#searchCity").addEventListener("submit", handleSubmit);
+document
+  .querySelector("#currentLocation")
+  .addEventListener("click", handleClickCurrentBtn);
